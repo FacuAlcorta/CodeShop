8864e6b3c3ee1b4ca16d04d55354ee2d65fdc6e6
Facu A <kornerito@gmail.com>
2022-08-01 23:03:43 -0300

    Cart widget interactivo

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,9 +1,13 @@
 import Badge from '@mui/material/Badge';
 import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
+import { useContext } from 'react';
+import { CartContext } from './CartContext';
 
 const CartWidget= () =>{
+    const global = useContext(CartContext);
+
     return(
-        <Badge badgeContent={1} color="primary">
+        <Badge badgeContent={global.cantItemTotal()} color="primary">
             <ShoppingCartCheckoutIcon />
         </Badge>
     );
